92e505fb2947384b9c5f460acd08f375ac313bca
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2022-07-24 22:58:27 +0500

    removed extra code and comments

--- cep_os.py
+++ cep_os.py
@@ -1,8 +1,6 @@
-
 import threading
 import time
 import random
-# import thread
 
 isTobacco = False
 isMatch = False
@@ -23,7 +21,10 @@ class thread_tobacco(threading.Thread):
 
         mutex.acquire()
         if(isTobacco):
+            print()
             print("The Tobacco thread has now locked the process")
+            print("Inside Tobacco: ")
+            cigaretteMaking()
 
             # timeToSleep = random.randint(1, 5)
             timeToSleep = 3
@@ -46,7 +47,11 @@ class thread_paper(threading.Thread):
         mutex.acquire()
 
         if(isPaper):
+            print()
             print("The Paper thread has now locked the process")
+            print("Inside Paper: ")
+
+            cigaretteMaking()
 
             timeToSleep = 3
             time.sleep(timeToSleep)
@@ -65,7 +70,10 @@ class thread_match(threading.Thread):
 
         mutex.acquire()
         if(isMatch):
+            print()
             print("The Match thread has now locked the process")
+            print("Inside Match:")
+            cigaretteMaking()
 
             timeToSleep = 3
             time.sleep(timeToSleep)
@@ -102,6 +110,8 @@ def itemsOnTable():
         print("Items on table:", ingredientsList[i])
         tableItemsList.append(ingredientsList[i])
     # mutex.release()
+    print()
+    print("Out of the Total Ingredients: ")
     return (tableItemsList)
 
 
@@ -134,7 +144,7 @@ def conditionCheck():
           smokerPaper())
 
     if smokerTobacco() not in varItemsOnTable:
-        print("Smoker having Tobacco is not on table so it's Process will be called to make the combination of cigarette")
+        print("\nTobacco is not on table so it's Process will be called to make the combination of cigarette")
         isTobacco = True
 
         return (isTobacco)
@@ -142,7 +152,7 @@ def conditionCheck():
         # print(itemsOnTable())
 
     if smokerPaper() not in varItemsOnTable:
-        print("Smoker having Paper is not on table so it's Process will be called to make the combination of cigarette")
+        print("\nPaper is not on table so it's Process will be called to make the combination of cigarette")
         isPaper = True
 
         return (isPaper)
@@ -150,18 +160,15 @@ def conditionCheck():
         # print(itemsOnTable())
 
     if smokerMatch() not in varItemsOnTable:
-        print("Smoker having Match is not on table so it's Process will be called to make the combination of cigarette")
+        print("\nMatch is not on table so it's Process will be called to make the combination of cigarette")
         isMatch = True
 
         return (isMatch)
 
-    # threads()
-
-    # print(itemsOnTable())
-
 
 def threads():
     commonMethod()
+
     if(isTobacco):
         t1_tobacco.start()
         t2_paper.start()
@@ -177,12 +184,13 @@ def threads():
         t1_tobacco.start()
         t2_paper.start()
 
-    print("Total Number of Running Threads: ", threading.active_count())
+    print()
+    # print("Total Number of Running Threads: ", threading.active_count())
 
     t1_tobacco.join()
     t2_paper.join()
     t3_match.join()
-    print("bye")
+    print("End Of Iteration")
 
     return ("")
 
@@ -191,11 +199,8 @@ def executionMutex(ingredientBoolean):
 
     global isPaper, isTobacco, isMatch
     global mutex
-    # mutex.acquire()
 
     threads()
-
-    # mutex.release()
 
     return (True)
 
@@ -204,80 +209,22 @@ def commonMethod():
     global mutex
     global t1_tobacco, t2_paper, t3_match
 
-    # mutex.acquire()
-    print(isTobacco, isPaper, isMatch)
-
     if isTobacco == True:
         print("Process Tobacco will run")
-        # t2_paper.join()
-        # t3_match.join()
-
-        # cigaretteMaking()
-        # isTobacco = False
 
     if isPaper == True:
         print("Process Paper will run")
-        # cigaretteMaking()
-        # t1_tobacco.join()
-        # t3_match.join()
 
     if isMatch == True:
         print("Process Match will run")
-        # cigaretteMaking()
-        # t1_tobacco.join()
-        # t2_paper.join()
-
-    # mutex.release()
 
 
 def cigaretteMaking():
 
     print("I am making a cigarette")
-    print("Cigarette has been Made")
+    print("Cigarette has been Made\n")
     return (True)
 
-    # mutex.acquire()
-    # t1 = thread_one()
-    # t2 = thread_two()
-    # t3 = thread_three()
-    # print(threading.active_count())
-    # t1.start()
-    # t2.start()
-    # t3.start()
 
-    # print("\n", threading.active_count())
-    # separately generating items on table
-    # a = generateRandomItems()
-    # print(a)
-    # b = itemsOnTable()
-    # print(b)
-    # smoker1 = smokerTobacco()
-    # smoker2 = smokerPaper()
-    # smoker3 = smokerMatch()
-    # print(smoker1, smoker2, smoker3)
-
-
-# def Agent():
-#     a = executionMutex(conditionCheck())
-#     print(a)
-
-
-# Agent()
-
-
-# mutex.acquire()
-# threads()
-# t1 = thread_one()
-# t2 = thread_two()
-# t3 = thread_three()
 varItemsOnTable = conditionCheck()
 a = executionMutex(varItemsOnTable)
-# commonMethod()
-# # print(a)
-
-
-# t1.start()
-# t2.start()
-# t3.start()
-
-# print(threading.active_count())
